d8aab7225998f395515c49c0aa236ff39df65d34
anil.senocak <anil.senocak@orioninc.com>
2022-10-08 11:36:06 +0300

    ApplicationReadyEvent

--- spring-kotlin/src/main/kotlin/com/github/senocak/BlogApplication.kt
+++ spring-kotlin/src/main/kotlin/com/github/senocak/BlogApplication.kt
@@ -5,11 +5,18 @@ import com.github.senocak.util.AppConstants.getLogger
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationListener
+import org.springframework.context.event.EventListener
 
 @SpringBootApplication
-class BlogApplication
+class BlogApplication {
+    @EventListener(value = [ApplicationReadyEvent::class])
+    fun init() {
+        getLogger().debug("[ApplicationReadyEvent]: app is ready")
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplicationBuilder(BlogApplication::class.java)
