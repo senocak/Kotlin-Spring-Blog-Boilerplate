b425c8b208254396f91a88cb72dc93687011fcd1
Anil Senocak <anil.senocak@turkcell.com.tr>
2022-12-20 10:20:57 +0300

    UserDataJpaTest updated

--- spring-kotlin/src/test/kotlin/com/github/senocak/domain/dto/UserDataJpaTest.kt
+++ spring-kotlin/src/test/kotlin/com/github/senocak/domain/dto/UserDataJpaTest.kt
@@ -8,12 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @DataJpaTest
 @ExtendWith(SpringExtension::class)
 @ActiveProfiles(value = ["datajpa-test"])
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+/*
+@TestPropertySource(properties = [
+    "spring.datasource.url=jdbc:tc:mysql:8.0.1://localhost/spring?TC_INITSCRIPT=file:src/test/resources/db.sql",
+    "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
+    "spring.liquibase.enabled=false",
+    "spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect",
+    "spring.jpa.hibernate.ddl-auto=validate"
+])
+*/
 class UserDataJpaTest {
     @Autowired
     private lateinit var userRepository: UserRepository
